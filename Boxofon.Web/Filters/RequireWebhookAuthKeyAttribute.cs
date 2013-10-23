5a671d3a86d60181f45aa1af951c1aac1e9adaea
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-10-23 09:48:48 +0200

    Implemented a simple call handler.

---
+++ Boxofon.Web/Filters/RequireWebhookAuthKeyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Boxofon.Web.Filters
+{
+    public class RequireWebhookAuthKeyAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return httpContext.Request.QueryString["authKey"] == WebConfigurationManager.AppSettings["WebhookAuthKey"];
+        }
+    }
+}
---
+++ Boxofon.Web/Filters/ValidateTwilioRequestAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Configuration;
+using Twilio.TwiML.Mvc;
+
+namespace Boxofon.Web.Filters
+{
+    public class ValidateTwilioRequestAttribute : ValidateRequestAttribute
+    {
+        public ValidateTwilioRequestAttribute() : base(WebConfigurationManager.AppSettings["twilio:AuthToken"])
+        {
+        }
+    }
+}
