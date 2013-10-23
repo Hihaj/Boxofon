0a0ce5b3bdbf36331e0278e7d60d2a7d47de2cb9
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-10-23 11:11:14 +0200

    Recreated web project.

---
+++ Boxofon.Web/Controllers/VoiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Boxofon.Web.Filters;
+using Twilio.Mvc;
+using Twilio.TwiML;
+using Twilio.TwiML.Mvc;
+
+namespace Boxofon.Web.Controllers
+{
+    public class VoiceController : Controller
+    {
+        [RequireWebhookAuthKey]
+        [HttpPost]
+        [ValidateTwilioRequest]
+        public ActionResult Incoming(VoiceRequest request)
+        {
+            var response = new TwilioResponse();
+            response.Say("Connecting you now.");
+            response.Dial(WebConfigurationManager.AppSettings["MyPhoneNumber"]);
+            return new TwiMLResult(response);
+        }
+    }
+}
