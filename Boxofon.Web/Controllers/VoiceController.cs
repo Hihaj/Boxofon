9d7f8462fea148ad13251accadb847a3631c821e
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-10-25 09:55:45 +0200

    Voicemail is working, remove test logic.

--- Boxofon.Web/Controllers/VoiceController.cs
+++ Boxofon.Web/Controllers/VoiceController.cs
@@ -29,42 +29,29 @@ namespace Boxofon.Web.Controllers
         public ActionResult Incoming(VoiceRequest request)
         {
             var response = new TwilioResponse();
-            response.SayInSwedish("Du ringer från ett svartlistat nummer och kommer inte kopplas fram. Om du vill kan du lämna ett meddelande efter tonen.");
-            response.Record(new
+            if (request.From == WebConfigurationManager.AppSettings["MyPhoneNumber"])
             {
-                action = Url.Action("VoiceMail", "Voice", new { authKey = WebConfigurationManager.AppSettings["WebhookAuthKey"] }),
-                method = "POST",
-                timeout = 5,
-                maxLength = 180,
-                playBeep = true,
-                finishOnKey = "#"
-            });
+                response.Say("Välkommen!", new { voice = "alice", language = "sv-SE" });
+            }
+
+            if (_phoneNumberBlacklist != null && _phoneNumberBlacklist.Contains(request.From))
+            {
+                response.Say("Du ringer från ett svartlistat nummer och kommer inte kopplas fram. Om du vill kan du lämna ett meddelande efter tonen.", new { voice = "alice", language = "sv-SE" });
+                response.Record(new
+                {
+                    action = Url.Action("VoiceMail", "Voice", new { authKey = WebConfigurationManager.AppSettings["WebhookAuthKey"] }),
+                    method = "POST",
+                    timeout = 5,
+                    maxLength = 180,
+                    playBeep = true,
+                    finishOnKey = "#"
+                });
+            }
+            else
+            {
+                response.Dial(WebConfigurationManager.AppSettings["MyPhoneNumber"]);
+            }
             return new TwiMLResult(response);
-
-            //var response = new TwilioResponse();
-            //if (request.From == WebConfigurationManager.AppSettings["MyPhoneNumber"])
-            //{
-            //    response.Say("Välkommen!", new { voice = "alice", language = "sv-SE" });
-            //}
-
-            //if (_phoneNumberBlacklist != null && _phoneNumberBlacklist.Contains(request.From))
-            //{
-            //    response.Say("Du ringer från ett svartlistat nummer och kommer inte kopplas fram. Om du vill kan du lämna ett meddelande efter tonen.", new { voice = "alice", language = "sv-SE" });
-            //    response.Record(new
-            //    {
-            //        action = Url.Action("VoiceMail", "Voice", new { authKey = WebConfigurationManager.AppSettings["WebhookAuthKey"] }),
-            //        method = "POST",
-            //        timeout = 5,
-            //        maxLength = 180,
-            //        playBeep = true,
-            //        finishOnKey = "#"
-            //    });
-            //}
-            //else
-            //{
-            //    response.Dial(WebConfigurationManager.AppSettings["MyPhoneNumber"]);
-            //}
-            //return new TwiMLResult(response);
         }
 
         [RequireWebhookAuthKey]
@@ -72,10 +59,9 @@ namespace Boxofon.Web.Controllers
         [ValidateTwilioRequest]
         public ActionResult VoiceMail(VoiceRequest request)
         {
-            // TODO Send an e-mail to the user saying that there is a new voicemail.
             try
             {
-                var mailgunResponse = "https://api.mailgun.net/v2/{0}/messages"
+                "https://api.mailgun.net/v2/{0}/messages"
                     .Fmt(WebConfigurationManager.AppSettings["mailgun:Domain"])
                     .PostToUrl(new
                     {
