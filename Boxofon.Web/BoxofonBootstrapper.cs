f0ece9cff23cfb4aedb2f2c42dfb3d8fae326422
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-10-29 16:47:26 +0100

    Fixed config error, added Nancy diagnostics dashboard config.

---
+++ Boxofon.Web/BoxofonBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Configuration;
+using Nancy;
+using Nancy.Diagnostics;
+
+namespace Boxofon.Web
+{
+    public class BoxofonBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override DiagnosticsConfiguration DiagnosticsConfiguration
+        {
+            get { return new DiagnosticsConfiguration { Password = WebConfigurationManager.AppSettings["nancy:DiagnosticsPassword"] }; }
+        }
+    }
+}
