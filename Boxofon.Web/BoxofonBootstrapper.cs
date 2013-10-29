fe34b8ed3dc8bf17de80e2cd8c6a03272e823915
Anders Fjeldstad <anders.fjeldstad@gmail.com>
2013-10-29 17:46:57 +0100

    Added logging.

--- Boxofon.Web/BoxofonBootstrapper.cs
+++ Boxofon.Web/BoxofonBootstrapper.cs
@@ -1,14 +1,30 @@
 ﻿using System.Web.Configuration;
+using NLog;
 using Nancy;
+using Nancy.Bootstrapper;
 using Nancy.Diagnostics;
+using Nancy.TinyIoc;
 
 namespace Boxofon.Web
 {
     public class BoxofonBootstrapper : DefaultNancyBootstrapper
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         protected override DiagnosticsConfiguration DiagnosticsConfiguration
         {
             get { return new DiagnosticsConfiguration { Password = WebConfigurationManager.AppSettings["nancy:DiagnosticsPassword"] }; }
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            pipelines.OnError += (ctx, ex) =>
+            {
+                Logger.ErrorException(ex.Message, ex);
+                return null;
+            };
         }
     }
 }
